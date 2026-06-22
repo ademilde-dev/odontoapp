39c21e9f4ddb131a2d894a5e6cadc809f4a4b0b4
Ademilde Martins Lanes <ademildelanes@yahoo.com.br>
2026-06-21 22:45:38 -0300

    Salvando alteracoes

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,10 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+<<<<<<< HEAD
 /// <reference path="./.next/types/routes.d.ts" />
+=======
+import "./.next/types/routes.d.ts";
+>>>>>>> 2ce7f07 (Ajustes no next.config.js e correcoes de build)
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
---
+++ next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  output: 'standalone',
+  transpilePackages: ['motion'],
+  turbopack: {}, // habilita Turbopack sem erro
+  webpack: (config, { dev }) => {
+    if (dev && process.env.DISABLE_HMR === 'true') {
+      config.watchOptions = {
+        ignored: /.*/,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
